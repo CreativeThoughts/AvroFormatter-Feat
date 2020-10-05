808be91a48bb1d76fd54e265396932b72c954a09
Sunil Mathew <sunilmathew.ai@gmail.com>
2020-10-05 21:04:59 +1100

    initial-commit

---
+++ AvroFormatter-Feat/CustomFormatter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AvroFormatterFeat
+{
+    public class CustomFormatter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+
+            JObject itemObj = new JObject();
+            itemObj.Add("string", JToken.FromObject(value));
+            itemObj.WriteTo(writer);
+
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            string val = (string)reader.Value;
+            return val;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(string);
+        }
+    }
+
+}
+
+
---
+++ AvroFormatter-Feat/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace AvroFormatterFeat
+{
+    class MainClass
+    {
+
+        static void Main(string[] args)
+        {
+            //Event Object replica of Fenergo DataModel
+            StatusEvent stEvent = new StatusEvent();
+
+            //Initiating the values that is manadatory and available
+            stEvent.CaseType = "KYCRefresh";
+            stEvent.PreviousStatus = "Initiated";
+
+            //newtonsoft json serialiser serialising the event object
+            string str = JsonConvert.SerializeObject(stEvent, Formatting.Indented);
+
+            //output the json onto Console
+            str = str.Replace("\\", string.Empty);
+            Console.WriteLine(str);
+        }
+
+
+        public class StatusEvent
+        {
+            public string StatusType { get; set; }
+
+            //decorating the porperty that would need to adhere to the avro schema format
+            // "previousStatus":{"string":"some"}
+            [JsonConverter(typeof(CustomFormatter))]
+            public string PreviousStatus { get; set; }
+
+            //"caseType:"sksm";
+            public string CaseType { get; set; }
+
+            [JsonConverter(typeof(CustomFormatter))]
+            public int NoOfTasksOpen { get; set; }
+
+            [JsonConverter(typeof(CustomFormatter))]
+            public string PreviousCase { get; set; }
+        }
+    }
+}
